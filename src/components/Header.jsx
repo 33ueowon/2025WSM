58380f689056454155256bbf07d41a01b4ad9daa
33ueowon <w2308@e-mirim.hs.kr>
2025-04-29 10:24:55 +0900

    header indent

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 
 function Header() {
-  const [isVisible, setIsVisible] = useState(true); // ture → true
+  const [isVisible, setIsVisible] = useState(true);
   const lastScrollY = useRef(0);
 
   useEffect(() => {
@@ -25,16 +25,17 @@ function Header() {
   return (
     <header
       style={{
-        position:'sticky',
-        top:0,
+        position: 'sticky',
+        top: 0,
         backgroundColor: 'white',
         transition: 'transform 0.3s ease',
-        transform:isVisible?'translateY(0)': 'translateY(-100%)',
-        zIndex:1000,
-        padding:'20px',
-        textAlign:'center',
-        boxShadow:'0 2px 4px rgba(0,0,0,0.1)'
-      }}>
+        transform: isVisible ? 'translateY(0)' : 'translateY(-100%)',
+        zIndex: 1000,
+        padding: '20px',
+        textAlign: 'center',
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+      }}
+    >
       <h1>김이한 & 이여원</h1>
       <p>2025년 10월 20일 21시</p>
       <p>63 빌딩 웨딩홀</p>
